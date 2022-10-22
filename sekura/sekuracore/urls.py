1d8a37ad5f68f7eaaa6856ebbf52906cd071c2d3
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2022-10-22 20:50:30 +0200

    fix tests

--- sekura/sekuracore/urls.py
+++ sekura/sekuracore/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("user/<int:pk>", views.UserDetails.as_view(), name="user"),
     path("user/", views.UserDetails.as_view(), name="currentuser"),
-    path("integrationavailable/", views.AvailableIntegrationList.as_view(), name="availableintegrations"),
+    path("integration/available/", views.AvailableIntegrationList.as_view(), name="availableintegrations"),
     path("organization/<int:orgid>/integration/", views.IntegrationList.as_view(), name="integration"),
     path("organization/<int:orgid>/integration/<int:pk>/", views.Integration.as_view(), name="integration"),
 ]
